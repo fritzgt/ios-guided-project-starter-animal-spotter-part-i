cd74a1148742af0c013a4d75750bcccbdf29522d
Fritz <fritz@icloud.com>
2022-04-21 20:39:46 -0500

    complete logic for  sign up method

--- AnimalSpotter/Controllers/APIController.swift
+++ AnimalSpotter/Controllers/APIController.swift
@@ -18,6 +18,7 @@ final class APIController {
     
     enum NetworkError: Error {
         case noData
+        case failedSignUp
     }
     
     // API docs: https://github.com/bloominstituteoftechnology/ios-animal-spotter-api
@@ -28,7 +29,34 @@ final class APIController {
     
     // create function for sign up
     func signUp(with user: User, completion: @escaping (Result<Bool, NetworkError>) -> Void) {
+        var request = postRequest(for: baseURL)
         
+        do {
+            //Convert the JSON data into an string
+            let jsonData = try JSONEncoder().encode(user)
+            print(String(data: jsonData, encoding: .utf8)!)
+            request.httpBody = jsonData
+            
+            URLSession.shared.dataTask(with: request) { _, response, error in
+                if let error = error {
+                    print("Sign up failed with error: \(error)")
+                    completion(.failure(.failedSignUp))
+                    return
+                }
+                
+                guard let response = response as? HTTPURLResponse, response.statusCode == 200  else {
+                    print("Sign up was unsucessful")
+                    completion(.failure(.failedSignUp))
+                    return
+                }
+
+                completion(.success(true))
+                
+            }.resume()
+            
+        } catch  {
+            print("🚨 \(error)")
+        }
     }
     
     // Helper method for posting
