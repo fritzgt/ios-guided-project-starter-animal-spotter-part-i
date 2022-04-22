f9611c8aa47d25db22c3d7f5fc3c2c374af28276
Fritz <fritz@icloud.com>
2022-04-21 20:44:26 -0500

    create model for bearer

---
+++ AnimalSpotter/Model/Bearer.swift
@@ -0,0 +1,14 @@
+//
+//  Bearer.swift
+//  AnimalSpotter
+//
+//  Created by FGT MAC on 4/21/22.
+//  Copyright © 2022 BloomTech. All rights reserved.
+//
+
+import Foundation
+
+
+struct Bearer: Codable {
+    let token: String
+}
